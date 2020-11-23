51eebc3f2a4488d9c12416e59c732bd9d72d77e0
Vishal Garg <vishugarg10004@gmail.com>
2020-11-24 00:41:12 +0530

    [Vishal] adding method to get data from local storage and add in table

--- js/payrollhome.js
+++ js/payrollhome.js
@@ -1,14 +1,21 @@
+let empPayrollList;
 window.addEventListener('DOMContentLoaded',(event)=>
 {
+    empPayrollList= getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent= empPayrollList.length;
     createInnerHtml();
 });
+const getEmployeePayrollDataFromStorage= ()=>{
+    return localStorage.getItem('EmployeePayrollList')?JSON.parse(localStorage.getItem('EmployeePayrollList')):[];
+}
 
 const createInnerHtml=()=>
 {
+    if(empPayrollList.length==0) return;
     const headerHtml= "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>"
     //using template literal
     let innerHtml= `${headerHtml}`;
-    let empPayrollList= createEmployeePayrollJSON();
+    //let empPayrollList= createEmployeePayrollJSON();
     for(const empPayrollData of empPayrollList){
     innerHtml= `${innerHtml}
     <tr>
@@ -20,10 +27,10 @@ const createInnerHtml=()=>
           <td>${empPayrollData._salary}</td>
           <td>${empPayrollData._startDate}</td>
           <td><img id="${empPayrollData._id}" onclick= "remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
-            <img id="${empPayrollData.id}" onclick= "update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg"></td>
+            <img id="${empPayrollData._id}" onclick= "update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg"></td>
     </tr>`;
     }
-    document.querySelector('#table-display').innerHTML=innerHTML;
+    document.querySelector('#table-display').innerHTML=innerHtml;
 }
 
 const createEmployeePayrollJSON = () => {
